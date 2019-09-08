26aa606e83ce46d2722bd64f7331c298abdf8f7d
jimengdie <741096458@qq.com>
2019-09-08 14:48:26 +0800

    add comment

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,12 +11,14 @@ Page({
     todo:[
     ]
   },
+  // 离开程序将数据写入缓存
   onHide() {
     wx.setStorage({
       key: 'todo',
       data: this.data.todo,
     })
   },
+  // 进入程序加载缓存
   onShow() {
     var todoStor = []
     try {
@@ -32,9 +34,11 @@ Page({
       todo:todoStor
     })
   },
+  // 添加数据
   handleBlur(res){
     this.addTodo(res)
   },
+  // addMore暂无使用 input数据不能为空 添加数据
   addTodo(res) {
     var msg = res.detail.value
     if(msg){
@@ -55,18 +59,18 @@ Page({
       })
     }
   },
-  radioChange(res) {
-    console.log(res)
-  },
+
   addNew(res) {
     this.setData({
       addMore:true
     })
   },
+  // 点击表示完成
   checkboxChange(res) {
     var haveDone = res.detail.value[0]
     this.doneItem(res.detail.value[0])
   },
+  // 完成内容点击后通过数据index找到对象将done改为true表示完成
   doneItem(itemIndex) {
     var newList = this.data.todo
     if (newList[itemIndex]){
@@ -79,8 +83,8 @@ Page({
       })
     }
   },
+  // 点击修改样式切换显示数据 在一个page根据点击不同展示不同数据
   changeShow(res) {
-  
     var color = this.data.changeColor
     var change = this.data.changedColor
     this.setData({
@@ -89,6 +93,7 @@ Page({
       showDone: !this.data.showDone
     })
   },
+  // 清空已完成数据 
   resetDone() {
     var done = this.data.todo
     var newTodo = []
